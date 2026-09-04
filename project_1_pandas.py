b128ccff8f9626a0e0d0b7b118aa1d4a95f890e5
PHÚC LÊ <giaphuc00007@gmail.com>
2026-09-04 20:43:50 +0700

    Enhance match statistics calculations
    Added functionality to calculate match results and total goals based on home and away scores.

--- project_1_pandas.py
+++ project_1_pandas.py
@@ -15,3 +15,29 @@ print(df.isnull().sum())
 
 print("\n=== THỐNG KÊ MÔ TẢ ===")
 print(df.describe())
+
+df["date"] = pd.to_datetime(df["date"])
+
+df["gf"] = df["gf"].fillna(0)
+df["ga"] = df["ga"].fillna(0)
+
+
+df["home_goals"] = np.where(df["venue"] == "Home", df["gf"], df["ga"])
+df["away_goals"] = np.where(df["venue"] == "Home", df["ga"], df["gf"])
+
+
+df["total_goals"] = df["home_goals"] + df["away_goals"]
+
+def get_result(row):
+    if row["home_goals"] > row["away_goals"]:
+        return "Home Win"
+    elif row["home_goals"] < row["away_goals"]:
+        return "Away Win"
+    else:
+        return "Draw"
+
+
+df["match_result"] = df.apply(get_result, axis=1)
+
+print("=== TỶ LỆ KẾT QUẢ TRẬN ĐẤU ===")
+print(df["match_result"].value_counts(normalize=True) * 100)
